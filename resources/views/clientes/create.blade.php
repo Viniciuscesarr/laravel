3bb5e0f4cce0218ca31e316690077d3dbf39e40e
Dyemerson  Almeida <djemerson.almeida@gmail.com>
2023-09-05 23:23:20 -0300

    Ajustado inputs lado a lado

--- resources/views/clientes/create.blade.php
+++ resources/views/clientes/create.blade.php
@@ -6,29 +6,29 @@
 
     <div class="card">
         <div class="card-body">
-            <form action="{{ route('clientes.store') }}" method="POST">
+            <form action="{{ route('clientes.store') }}" method="POST" class="row g-3">
                 @csrf
-                <div class="mb-3">
+                <div class="col-md-6 mb-3">
                     <label for="nome" class="form-label">Nome:</label>
                     <input type="text" class="form-control" id="nome" name="nome" required>
                 </div>
 
-                <div class="mb-3">
+                <div class="col-md-6 mb-3">
                     <label for="apelido" class="form-label">Apelido:</label>
                     <input type="text" class="form-control" id="apelido" name="apelido">
                 </div>
 
-                <div class="mb-3">
+                <div class="col-md-6 mb-3">
                     <label for="endereco" class="form-label">Endereço:</label>
                     <input type="text" class="form-control" id="endereco" name="endereco">
                 </div>
 
-                <div class="mb-3">
+                <div class="col-md-6 mb-3">
                     <label for="telefone" class="form-label">Telefone:</label>
                     <input type="tel" class="form-control" id="telefone" name="telefone">
                 </div>
 
-                <div class="d-grid gap-2">
+                <div class="col-12">
                     <button type="submit" class="btn btn-primary">CADASTRAR</button>
                 </div>
             </form>
