5dba3261a5664ee093bdf37e2ae6ce7a77f0d384
Vinicius <vinicius0277777@gmail.com>
2023-09-11 09:40:19 -0300

    Ajustes

--- resources/views/estoque/index.blade.php
+++ resources/views/estoque/index.blade.php
@@ -1,10 +1,12 @@
 @extends('layouts.default') <!-- Estende o layout padrão -->
 
 @section('content')
+
 <div class="container">
     @include('layouts.messages')
     <h1>Estoque</h1>
     <a href="#" class="btn btn-primary mb-3">Cadastrar Novo Produto</a>
+    <a href="#" class="buttonremove">Remover Produto</a>
 
     <div class="card">
         <div class="card-body">
